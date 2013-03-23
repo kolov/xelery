ba6daef745cadee373446560026e11402899d190
Assen Kolov <assen.kolov@gmail.com>
2013-03-23 20:16:17 +0100

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject xelery "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.akolov/xelery "0.1.0"
+  :description "XML Schema Reader"
+  :url "https://github.com/kolov/xelery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:resource-paths ["dev-data"]}}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :lein-release {:deploy-via :clojars}
+  :profiles {:dev {:resource-paths ["dev-data"]}
+              :user {:plugins [[lein-clojars "0.9.1"]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+ 
+)
