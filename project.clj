fe4e5476cbb42d7a5b578627be7354db1978b153
Assen Kolov <assen.kolov@gmail.com>
2013-10-19 20:53:08 +0200

    test2junit

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject com.akolov/xelery "0.4.3-SNAPSHOT"
-  :description "XML Schema Reader fro Clojure"
+  :description "XML Schema Reader for Clojure"
   :min-lein-version "2.0.0"
   :url "https://github.com/kolov/xelery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :profiles {:dev {:resource-paths ["dev-data"]}}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:resource-paths ["dev-data"]
+                   :plugins [[test2junit "1.0.1"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
 
   :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
                                      :creds :gpg}
