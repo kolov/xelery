00b0905fd3c0ae34ef2154e3282e653acdc38c64
Assen Kolov <assen.kolov@gmail.com>
2015-06-28 11:35:01 +0200

    other repo

--- project.clj
+++ project.clj
@@ -3,18 +3,22 @@
   :min-lein-version "2.0.0"
   :url "https://github.com/kolov/xelery"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
   :profiles {:dev {:resource-paths ["dev-data"]
-                   :plugins [[test2junit "1.0.1"]]}}
+                   :plugins        [[test2junit "1.0.1"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]]
 
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}
-                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]]
+
   :scm {:url "https://github.com/kolov/xelery.git"}
   :pom-addition [:developers [:developer [:name "Assen Kolov"]
                               [:email "assen.kolov@gmail.com"]
                               ]]
+  :repositories [
+                 ["snapshots" "http://nexus.akolov.com/content/repositories/snapshots"]
+                 ["releases" {
+                              :url           "http://nexus.akolov.com/content/repositories/releases"
+                              :sign-releases false
+                              }
+                  ]]
   )
